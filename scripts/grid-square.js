0a7b8c4edbf3c1c72a18f250b58eb44b022f8eb3
Todd Fleeman <rtoddf@comcast.net>
2017-12-08 12:07:54 -0500

    playing with gallery layouts

---
+++ scripts/grid-square.js
@@ -0,0 +1,14 @@
+// https://www.sitepoint.com/dom-manipulation-vanilla-javascript-no-jquery/
+
+document.querySelector('body').addEventListener('click', function(evt) {
+    if ( evt.target.parentNode.parentNode.classList.contains('tile') ) {
+
+		var tileDivs = document.getElementsByClassName('tile');
+
+        for(var i=0; i<tileDivs.length; i++){
+        	tileDivs[i].classList.remove('expanded-square');
+        }
+
+        evt.target.parentNode.parentNode.classList.add('expanded-square');
+    }
+}, true);
