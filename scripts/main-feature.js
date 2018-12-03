7a0a4c3e460b5cfa4f21dab53240757d8c6a52dd
Todd Fleeman <rtoddf@comcast.net>
2018-12-02 22:21:00 -0500

    cleanup and consolidation

--- scripts/main-feature.js
+++ scripts/main-feature.js
@@ -1,12 +1,8 @@
 var height
 
 $( document ).ready(function() {
-	height = ($('.content-holder').height() - $('.content-holder h2').height()) * -1
+	height = ($('.content-holder').height() - $('.content-holder h3').height()) * -1
 	$('.mainFeature').find('.content-holder').css('bottom', height)
-
-	// $('.mainFeature').each(function(i){
-	// 	console.log('this: ', $(this))
-	// })
 })
 
 $('.mainFeature').on('mouseover', function(){
